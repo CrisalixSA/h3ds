ff42dab0c66fdc2eb23c32c8a0740e0efc454c93
Eduard Ramon Maldonado <eduard.ramon.maldonado@gmail.com>
2021-08-02 13:04:47 +0200

    Minor refactor

--- h3ds/dataset.py
+++ h3ds/dataset.py
@@ -12,20 +12,18 @@ from utils import download_file_from_google_drive, md5
 
 class H3DS:
 
-    def __init__(self, path: str = None, token: str = None):
+    def __init__(self, path: str):
 
-        self.path = path or os.getenv('H3DS_PATH', None)
-        self.token = token or os.getenv('H3DS_TOKEN', None)
+        self.path = path
 
-        if self.path is None:
-            logger.exception('H3DS path not provided')
-
-        if self.token is None:
-            logger.exception('H3DS token not provided')
+        if not os.path.exists(self.path):
+            logger.warning(
+                f'{self.path} does not exist. Change the path or call H3DS.download.'
+            )
 
         self._load_config()
 
-    def pull(self):
+    def download(self, token):
         # Download zip file
         tmp_dir = os.path.join(self.path, 'tmp')
         if not os.path.exists(tmp_dir):
@@ -48,7 +46,7 @@ class H3DS:
         logger.print(f'Unzipping file to {self.path}')
         with zipfile.ZipFile(tmp_zip, 'r') as zip_ref:
             zip_ref.extractall(self.path,
-                pwd=self.token.encode('utf-8'))
+                pwd=token.encode('utf8'))
 
         # Remove temporal zip
         logger.print(f'Removing temporary files')
@@ -57,16 +55,16 @@ class H3DS:
     def scenes(self):
         return self._config['scenes']
 
-    def view_configs(self, scene_id: str):
+    def default_views_configs(self, scene_id: str):
         return list(self._config['views'][scene_id].keys())
 
-    def get_scene(self, scene_id: str, view_config_id: str = None):
+    def load_scene(self, scene_id: str, views_config_id: str = None):
         scene_path = os.path.join(self.path, scene_id)
 
         mesh = self.get_mesh(scene_id)
-        images = self.get_images(scene_id, view_config_id)
-        masks = self.get_masks(scene_id, view_config_id)
-        cameras = self.get_cameras(scene_id, view_config_id)
+        images = self.get_images(scene_id, views_config_id)
+        masks = self.get_masks(scene_id, views_config_id)
+        cameras = self.get_cameras(scene_id, views_config_id)
 
         return mesh, images, masks, cameras
 
@@ -75,20 +73,20 @@ class H3DS:
 
         return trimesh.load(mesh_path, process=False)
 
-    def get_images(self, scene_id: str, view_config_id: str = None):
+    def get_images(self, scene_id: str, views_config_id: str = None):
         images = self._get_images(scene_id, 'image')
 
-        return self._filter_views(images, scene_id, view_config_id)
+        return self._filter_views(images, scene_id, views_config_id)
 
-    def get_masks(self, scene_id: str, view_config_id: str = None):
+    def get_masks(self, scene_id: str, views_config_id: str = None):
         masks = self._get_images(scene_id, 'mask')
 
-        return self._filter_views(masks, scene_id, view_config_id)
+        return self._filter_views(masks, scene_id, views_config_id)
 
-    def get_cameras(self, scene_id: str, view_config_id: str = None):
+    def get_cameras(self, scene_id: str, views_config_id: str = None):
         cameras = self._get_images(scene_id, 'mask')
 
-        return self._filter_views(cameras, scene_id, view_config_id)
+        return self._filter_views(cameras, scene_id, views_config_id)
 
     def _load_config(self):
         h3ds_dir = os.path.dirname(os.path.realpath(__file__))
@@ -111,10 +109,10 @@ class H3DS:
         images_paths = sorted(list(glob.glob(os.path.join(images_dir, '*.jpg'))))
         return [Image.open(img).copy() for img in images_paths]
 
-    def _filter_views(self, elements, scene_id: str, view_config_id: str = None):
+    def _filter_views(self, elements, scene_id: str, views_config_id: str = None):
 
-        if view_config_id is None:
+        if views_config_id is None:
             return elements
 
-        view_config = self._get_views_config(scene_id, view_config_id)
-        return [elements[idx] for idx in view_config]
+        views_config = self._get_views_config(scene_id, views_config_id)
+        return [elements[idx] for idx in views_config]
