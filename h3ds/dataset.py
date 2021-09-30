61a1a76d1ac6ae8d333ad05f9ed8d486e62c7e88
Eduard Ramon Maldonado <eduard.ramon.maldonado@gmail.com>
2021-09-30 22:19:06 +0200

    Updating documentation

--- h3ds/dataset.py
+++ h3ds/dataset.py
@@ -112,7 +112,12 @@ class H3DSHelper:
 class H3DS:
 
     def __init__(self, path: str, config_path: str = None):
-
+        """
+        Class to manage the data available in the H3DS dataset.
+        Args:
+            path        (str): Path to store the dataset locally.
+            config_path (str): Optional custom config file.
+        """
         self.path = os.path.expanduser(path)
         self.config_path = config_path or ConfigsHelper.get_config_file(
             'config')
@@ -125,7 +130,15 @@ class H3DS:
             )
 
     def download(self, token, force=False):
-
+        """
+        Downloads the dataset to the specified path in the __init__ method. The dataset
+        is download only if it is not available or if the flag force is True.
+        Args:
+            token  (str): H3DS token
+            force (bool): Flag to force the download
+        Returns:
+            None
+        """
         # Check if dataset is already available
         if self.is_available() and not force:
             logger.info('Dataset already available. Skipping download')
@@ -160,19 +173,56 @@ class H3DS:
         shutil.rmtree(tmp_dir)
 
     def is_available(self):
+        """
+        Checks if a valid version of the dataset is available at the specified path
+        Args:
+        Returns:
+            bool : True if available, otherwise false
+        """
         return self.helper.version_config() == self.helper.version_dataset() and \
             all([os.path.exists(f) for f in self.helper.files()])
 
     def scenes(self, tags: set = {}):
+        """
+        Specifies the available scenes in the dataset as scene identifiers. A scene identifier
+        can be used to load data for a concrete scene, for instance the mesh or the cameras.
+        If tags are specified, the list is filtered and only the scenes containing the tag are retuned.
+        If more than one tag is provided, only the scenes containing all the tags will be returned.
+        Args:
+            tags (set): Optional set of tags to filter the scenes, i.e. 'h3d-net'.
+        Returns:
+            list : List scene identifiers (str)
+        """
         return self.helper.scenes(tags)
 
     def default_views_configs(self, scene_id: str):
+        """
+        Specifies all the subsets of views (configs) available for a certain scene. Each
+        configuration of views is associated to a view_config_identifier.
+        Args:
+            scene_id (str): Scene identifier
+        Returns:
+            list : List of view config identifiers available for that scene.
+        """
         return self.helper.default_views_configs()
 
     def load_scene(self,
                    scene_id: str,
                    views_config_id: str = None,
                    normalized: bool = False):
+        """
+        Loads all the elements of a scene, which are the mesh, the images,
+        the masks and the cameras.
+        Args:
+            scene_id        (str): Scene identifier
+            views_config_id (str): Views configuration defining subset of views
+            normalized     (bool): Scene normalized to fit inside a unit sphere
+        Returns:
+            trimesh.Trimesh: The 3D geometry of the scene as a mesh
+            list : Array of the images
+            list : Array of the masks
+            list : Array of the cameras
+        """
         mesh = self.load_mesh(scene_id, normalized)
         images = self.load_images(scene_id, views_config_id)
         masks = self.load_masks(scene_id, views_config_id)
@@ -181,6 +231,14 @@ class H3DS:
         return mesh, images, masks, cameras
 
     def load_mesh(self, scene_id: str, normalized: bool = False):
+        """
+        Loads the mesh for a given scene as a trimesh.Trimesh.
+        Args:
+            scene_id    (str): Scene identifier
+            normalized (bool): Scene normalized to fit inside a unit sphere
+        Returns:
+            trimesh.Trimesh: The 3D geometry of the scene as a mesh
+        """
         mesh = trimesh.load(self.helper.scene_mesh(scene_id), process=False)
         if normalized:
             normalization_transform = self._load_normalization_transform(
@@ -190,11 +248,27 @@ class H3DS:
         return mesh
 
     def load_images(self, scene_id: str, views_config_id: str = None):
+        """
+        Loads the RGB images for a given scene as PIL.Image.
+        Args:
+            scene_id        (str): Scene identifier
+            views_config_id (str): Views configuration defining subset of views
+        Returns:
+            list : Array of the images
+        """
         images = self._load_images(self.helper.scene_images(scene_id))
 
         return self._filter_views(images, scene_id, views_config_id)
 
     def load_masks(self, scene_id: str, views_config_id: str = None):
+        """
+        Loads the binary masks for a given scene as PIL.Image.
+        Args:
+            scene_id        (str): Scene identifier
+            views_config_id (str): Views configuration defining subset of views
+        Returns:
+            list : Array of the masks
+        """
         masks = self._load_images(self.helper.scene_masks(scene_id))
 
         return self._filter_views(masks, scene_id, views_config_id)
@@ -203,6 +277,17 @@ class H3DS:
                      scene_id: str,
                      views_config_id: str = None,
                      normalized: bool = False):
+        """
+        Loads the cameras for a given scene. Each cameras is defined as a tupple
+        of two elements. The first one is a 3x3 np.ndarray matrix with the calibration
+        and the second is a 4x4 np.ndarray matrix with the camera pose.
+        Args:
+            scene_id        (str): Scene identifier
+            views_config_id (str): Views configuration defining subset of views
+            normalized     (bool): Scene normalized to fit inside a unit sphere
+        Returns:
+            list : Array of the cameras
+        """
         camera_dict = np.load(self.helper.scene_cameras(scene_id))
         if normalized:
             normalization_transform = self._load_normalization_transform(
@@ -219,24 +304,49 @@ class H3DS:
         return self._filter_views(cameras, scene_id, views_config_id)
 
     def load_normalization_matrix(self, scene_id: str):
-        '''
-        Transforms the scene from mm scale to the unit sphere
-        '''
+        """
+        Loads the transformation that normalizes the scene from mm to a unit sphere.
+        Args:
+            scene_id, (str): Scene identifier
+        Returns:
+            np.array : A 4x4 similarity transform
+        """
         return self._load_normalization_transform(scene_id).matrix
 
     def _load_normalization_transform(self, scene_id: str):
-        '''
-        Transforms the scene into a unit sphere
-        '''
+        """
+        Internal method: Loads the transformation that normalizes the scene
+        from mm to a unit sphere.
+        Args:
+            scene_id (str): Scene identifier
+        Returns:
+            AffineTransform : A 4x4 similarity transform
+        """
         camera_dict = np.load(self.helper.scene_cameras(scene_id))
         s = camera_dict['scale_mat_0'].astype(np.float32)
         t = AffineTransform(matrix=np.linalg.inv(s))
         return t
 
     def _load_images(self, images_paths: list):
+        """
+        Internal method: Loads a list of image as PIL.Image from their paths
+        Args:
+            images_paths (list): List of image paths
+        Returns:
+            list : List of images as PIL.Image
+        """
         return [Image.open(img).copy() for img in images_paths]
 
     def _get_views_config(self, scene_id: str, config_id: str):
+        """
+        Loads a list of list of view identifiers that is pre-defined
+        in the config.toml file.
+        Args:
+            scene_id (str): Scene identifier
+            config_id (str): Config identifier (see default_views_configs method)
+        Returns:
+            list : Array of view identifiers
+        """
         return self._config['scenes'][scene_id]['default_views_configs'][
             config_id]
 
@@ -244,7 +354,14 @@ class H3DS:
                       elements,
                       scene_id: str,
                       views_config_id: str = None):
-
+        """
+        Internal method: Filters a list of objects associated to views (images, cameras)
+        according to a scene and a configuration of views.
+        Args:
+            elements (list): List of all the images, masks or cameras from a scene
+        Returns:
+            list : Filtered list of elements that belong to the views defined by views_config_id
+        """
         if views_config_id is None:
             return elements
 
